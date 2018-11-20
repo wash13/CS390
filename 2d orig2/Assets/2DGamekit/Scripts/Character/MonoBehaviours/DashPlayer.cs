cc0b339021376af40c1fbdc12ba00e8991b97f7b
wash13 <wolfberserkr@gmail.com>
2018-11-20 00:02:12 -0500

    new stuff
    the new stuff I was working on but not unfinished swap

--- 2d orig2/Assets/2DGamekit/Scripts/Character/MonoBehaviours/DashPlayer.cs
+++ 2d orig2/Assets/2DGamekit/Scripts/Character/MonoBehaviours/DashPlayer.cs
@@ -9,22 +9,44 @@ public class DashPlayer : MonoBehaviour {
     private float dashTime;
     public float starDashTime;
     private int direction;
-    private Vector2 current;
+    //private Vector2 current;
     private Gamekit2D.Damageable dm;
+    private float prevGrav;
+    private Gamekit2D.PlayerCharacter pc;
+    bool dashing;
 
+    private float speed;
     // Use this for initialization
+
     void Start () {
         rb = GetComponent<Rigidbody2D>();
         dm = GetComponentInParent<Gamekit2D.Damageable>();
+        pc = GetComponentInParent<Gamekit2D.PlayerCharacter>();
         dashTime = starDashTime;
-        current = rb.position;
+        //current = rb.position;
+        //prevGrav = pc.gravity;
+
+        speed = pc.maxSpeed;
+        dashing = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (direction == 0 && Input.GetKeyDown(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.A))
         {
+            direction = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = 2;
+        }
 
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            dashing = true;
+            //speed = pc.maxSpeed;
+            //pc.gravity = 0;
+            /*
             if (Input.GetKey(KeyCode.A))
             {
                 direction = 1;
@@ -33,6 +55,7 @@ public class DashPlayer : MonoBehaviour {
             {
                 direction = 2;
             }
+            */
             /*
             else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
@@ -44,27 +67,33 @@ public class DashPlayer : MonoBehaviour {
             }
             */
         }
-        else
+        else if (dashing == true)
         {
             if(dashTime <= 0)
             {
-                direction = 0;
+                //direction = 0;
                 dashTime = starDashTime;
                 rb.velocity = Vector2.zero;
                 dm.DisableInvulnerability();
+
+                dashing = false;
+                pc.maxSpeed = speed;
+                //pc.gravity = prevGrav;
             }
             else
             {
-                dm.EnableInvulnerability();
+                dm.EnableInvulnerability(true);
                 dashTime -= Time.deltaTime;
                 if (direction == 1)
                 {
                     rb.AddForce(Vector3.left * dashSpeed);
+                    //pc.maxSpeed = speed * dashSpeed;
                     //rb.velocity = Vector2.left * dashSpeed;
                 }
                 else if (direction == 2)
                 {
                     rb.AddForce(Vector3.right * dashSpeed);
+                    //pc.maxSpeed = speed * dashSpeed;
                     //rb.velocity = Vector2.right * dashSpeed;
                 }
                 /*
