b06b88286b4c28b99c0cb53e7ebeb9307c463fce
octobusinesshub <jajjan@masonlive.gmu.edu>
2018-11-30 10:20:13 -0500

    new boss
    All I have for now

---
+++ 2d orig2/Assets/2DGamekit/Scripts/Character/MonoBehaviours/DashPlayer.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashPlayer : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float dashSpeed;
+    private float dashTime;
+    public float starDashTime;
+    private int direction;
+    //private Vector2 current;
+    private Gamekit2D.Damageable dm;
+    private float prevGrav;
+    private Gamekit2D.PlayerCharacter pc;
+    bool dashing;
+
+    private float speed;
+    // Use this for initialization
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        dm = GetComponentInParent<Gamekit2D.Damageable>();
+        pc = GetComponentInParent<Gamekit2D.PlayerCharacter>();
+        dashTime = starDashTime;
+        //current = rb.position;
+        //prevGrav = pc.gravity;
+
+        speed = pc.maxSpeed;
+        dashing = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            dashing = true;
+            //speed = pc.maxSpeed;
+            //pc.gravity = 0;
+            /*
+            if (Input.GetKey(KeyCode.A))
+            {
+                direction = 1;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                direction = 2;
+            }
+            */
+            /*
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                direction = 3;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                direction = 4;
+            }
+            */
+        }
+        else if (dashing == true)
+        {
+            if(dashTime <= 0)
+            {
+                //direction = 0;
+                dashTime = starDashTime;
+                rb.velocity = Vector2.zero;
+                dm.DisableInvulnerability();
+
+                dashing = false;
+                pc.maxSpeed = speed;
+                //pc.gravity = prevGrav;
+            }
+            else
+            {
+                dm.EnableInvulnerability(true);
+                dashTime -= Time.deltaTime;
+                if (direction == 1)
+                {
+                    rb.AddForce(Vector3.left * dashSpeed);
+                    //pc.maxSpeed = speed * dashSpeed;
+                    //rb.velocity = Vector2.left * dashSpeed;
+                }
+                else if (direction == 2)
+                {
+                    rb.AddForce(Vector3.right * dashSpeed);
+                    //pc.maxSpeed = speed * dashSpeed;
+                    //rb.velocity = Vector2.right * dashSpeed;
+                }
+                /*
+                else if (direction == 3)
+                {
+                    rb.velocity = Vector2.up * dashSpeed;
+                }
+                else if (direction == 4)
+                {
+                    rb.velocity = Vector2.down * dashSpeed;
+                }
+                */
+            }
+        }
+	}
+}
---
+++ 2d orig2/Assets/2DGamekit/Scripts/Character/MonoBehaviours/SwapPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapPlayer : MonoBehaviour
+{
+    public GameObject afterimage;
+    private bool telActive;
+    private Vector3 location;
+    private GameObject img;
+// private Gamekit2D.PlayerCharacter pc;
+
+    // Use this for initialization
+    void Start()
+    {
+        //pc = GetComponentInParent<Gamekit2D.PlayerCharacter>();
+        
+        telActive = false;
+        location = this.gameObject.transform.position;
+// location = pc.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (telActive == true)
+            {
+                // pc.transform.position = location;
+                this.gameObject.transform.position = location;
+                telActive = false;
+                Destroy(img);
+            }
+
+            else if (telActive == false)
+            {
+                // location = pc.transform.position;
+                location = this.gameObject.transform.position;
+                telActive = true;
+                img = Instantiate(afterimage, location, Quaternion.identity);
+            }
+        }
+    }
+}
